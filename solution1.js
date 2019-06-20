a82046d6ecbd0da6631ff3701080658217399a3b
Krishna <krushnareddy@vivartha.com>
2019-06-20 14:09:39 +0530

    Solution1

---
+++ solution1.js
@@ -0,0 +1,24 @@
+let books = [
+{   id: "1",
+    title: "React",
+    author: "ABC",
+    alreadyRead: false
+},
+{   id: "2",
+    title: "PHP",
+    author: "DEF",
+    alreadyRead: true
+
+},
+{   id: "3",
+    title: "JAVA",
+    author: "GHI",
+    alreadyRead:true
+
+},
+{   id: "4",
+    title: ".NET",
+    author: "JKL",
+    alreadyRead: false
+}
+]
